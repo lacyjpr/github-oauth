2cc4123251dfad180ed94d50a531d566b4e0b90b
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-24 10:04:43 -0800

    Add /api/current_user route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,4 +4,8 @@ module.exports = app => {
   app.get('/auth/github', passport.authenticate('github'));
 
   app.get('/auth/github/callback', passport.authenticate('github'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
