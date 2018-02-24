19f79c8e3416c7af73db44105856203fe162eda0
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-24 09:59:48 -0800

    Set up cookies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const passport = require('passport');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,7 +10,15 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+    keys: [keys.cookieKey],
+  })
+);
+
 app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
