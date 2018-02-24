4b287ec5e598c309708c05b6204c5391ed4578ed
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-23 22:44:35 -0800

    Get accessToken

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GitHubStrategy());
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: keys.githubClientID,
+      clientSecret: keys.githubClientSecret,
+      callbackURL: '/auth/github/callback',
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback', passport.authenticate('github'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
